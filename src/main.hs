fa46e78a38c7317887cdb8a4a41ee8c39febc45f
Mihai Maruseac <mihai.maruseac@gmail.com>
2023-01-09 21:47:34 -0800

    Initial sources
    Signed-off-by: Mihai Maruseac <mihai.maruseac@gmail.com>

---
+++ src/main.hs
@@ -0,0 +1,16 @@
+{-|
+Module: Main
+Description: The entry point of the site generator
+Copyright: (c) Mihai Maruseac 2023
+License: BSD-3-Clause
+Maintainer: mihai.maruseac@gmail.com
+Stability: experimental
+Portability: portable
+
+The entry point of the site generator.
+-}
+
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
