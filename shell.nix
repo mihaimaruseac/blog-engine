cbdb62b147af4bf5537951b27b3395268c8cd11f
Mihai Maruseac <mihai.maruseac@gmail.com>
2023-04-28 21:22:21 -0700

    Try a new version of `shell.nix` to update GHC.
    This version attempts to add `diagrams` (and `palette` by the time this got
    checked in) to the "system GHC" that is available to the Nix shell. The GHC
    controlled by Cabal has no issue, but `diagrams-builder` uses the system GHC so
    it needs to be able to find the needed packages.
    
    Signed-off-by: Mihai Maruseac <mihai.maruseac@gmail.com>

--- shell.nix
+++ shell.nix
@@ -1,12 +1,22 @@
 { pkgs ? import <nixpkgs> {} }:
-with pkgs;
-mkShell {
-  buildInputs = [
-    cabal-install
-    haskell.compiler.ghc925
-    zlib.dev
-  ];
-  shellHook = ''
-    export PS1="[\[\033[01;32m\]nix-shell\[\033[00m\]:\W] \[\033[01;32m\]λ\[\033[00m\] "
-  '';
-}
+
+let
+  localGHC = pkgs.haskellPackages.ghcWithPackages (ps: with ps; [
+    diagrams
+    palette
+    # diagrams-html5
+    # diagrams-svg
+  ]);
+in
+  with pkgs;
+  mkShell {
+    buildInputs = [
+      localGHC
+      cabal-install
+      zlib.dev
+    ];
+
+    shellHook = ''
+      export PS1="[\[\033[01;32m\]nix-shell\[\033[00m\]:\W] \[\033[01;32m\]λ\[\033[00m\] "
+    '';
+  }
